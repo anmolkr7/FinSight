c6bdd7483ec1066a656d291976569cd67a942d02
anmolkr7 <anmolojha952@gmail.com>
2025-05-17 21:37:16 +0530

    Placing dummy header component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/headers";
 
 const inter= Inter({subsets: ["latin"]});
 
@@ -13,8 +14,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         {/*header*/}
-        {children}
+        <Header></Header>
+        <main className="min-h-screen">{children}</main>
         {/*footer*/}
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made by Anmol</p>
+          </div>
+        </footer>
         </body>
     </html>
   );
