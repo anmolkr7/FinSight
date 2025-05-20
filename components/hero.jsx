eccf85ec68b581c662aeaa86fa679af7674caf8a
anmolkr7 <anmolojha952@gmail.com>
2025-05-20 23:26:10 +0530

    added banner and texts with gradient

---
+++ components/hero.jsx
@@ -0,0 +1,45 @@
+"use client";
+import React from 'react'
+import Link from 'next/link';
+import { Button } from './ui/button';
+import Image from 'next/image';
+const HeroSection = () => {
+  return (
+    <div className='pb-20 px-4'>
+        <div className='container mx-auto text-center'>
+           <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+          Manage Your Finances <br /> with Intelligence
+        </h1>
+            <p>
+                An AI-powered financial management platform that helps you track,
+                analyze, and optimize your spending habits with real-time insights.
+            </p>
+            <div>
+                <Link href="/dashboard">
+                    <Button size="lg" className="px-8">
+                        Get Started
+                        </Button>
+                </Link>
+                <Link href="/aakdhdf">
+                    <Button size="lg" variant="outline" className="px-8">
+                        Watch Demo
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <div>
+                    <Image src='/banner.jpeg'
+                    width={1280}
+                    height={720}
+                    alt='Dashboard Preview'
+                    className='rounded-lg shadow-2xl border mx-auto'
+                    priority
+                    ></Image>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
