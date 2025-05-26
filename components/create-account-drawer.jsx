c2e81973151b79c03d34b9e1c52306d746a9b29d
anmolkr7 <anmolojha952@gmail.com>
2025-05-26 17:56:58 +0530

    create drawer contents: name and actype added

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -8,9 +8,24 @@ import {
   DrawerTrigger,
   DrawerClose,
 } from "@/components/ui/drawer";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { accountSchema } from "@/app/lib/schema";
+import { Input } from "./ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+
 const CreateAccountDrawer = ({children}) => {
     const [open,setOpen]=useState(false);
-
+    const {register,handleSubmit,formState:{errors},
+    setValue,watch,reset}=useForm({
+        resolver: zodResolver(accountSchema),
+        defaultValues: {
+            name: "",
+            type: "CURRENT",
+            balance: "",
+            isDefault: false,
+        },
+    })
   return (
     <Drawer open={open} onOpenChange={setOpen}>
         <DrawerTrigger asChild>{children}</DrawerTrigger>
@@ -18,9 +33,35 @@ const CreateAccountDrawer = ({children}) => {
             <DrawerHeader>
                 <DrawerTitle>Create New Account</DrawerTitle>
             </DrawerHeader>
-            <div>
-                <form action="">
-                    
+            <div className="px-4 pb-4">
+                <form className="space-y-4">
+
+
+                    <div className="space-y-2">
+                        <label htmlFor="name" className="text-sm font-medium">Account Name</label>
+                        <Input id="name" placeholder="e.g.,Main Checking"
+                        {...register("name")}
+                        ></Input>
+                        {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+                    </div>
+
+
+                     <div className="space-y-2">
+                        <label htmlFor="type" className="text-sm font-medium">Account Type</label>
+                        <Select onValueChange={(value) => setValue("type", value)}
+                            defaultValue={watch("type")}>
+                            <SelectTrigger id="type">
+                                <SelectValue placeholder="Select Type" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="CURRENT">Current</SelectItem>
+                                <SelectItem value="SAVINGS">Savings</SelectItem>
+                            </SelectContent>
+                        </Select>
+                        {errors.type && <p className="text-red-500 text-sm">{errors.type.message}</p>}
+                    </div>
+
+
                 </form>
             </div>
         </DrawerContent>
