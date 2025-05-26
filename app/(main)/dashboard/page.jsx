053b787c78703f1a6ec70273891b3a0b8af8169f
anmolkr7 <anmolojha952@gmail.com>
2025-05-26 14:24:35 +0530

    app routes

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div className='px-5'>
+        {/* BudgetProgress */}
+
+
+        {/* Dashboard Overview */}
+
+
+        {/* Accounts Grid */}
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default DashboardPage
