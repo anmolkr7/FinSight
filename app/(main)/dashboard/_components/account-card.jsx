9c24b76859308d1bff54f8880296971ceb62a4e0
anmolkr7 <anmolojha952@gmail.com>
2025-05-28 17:37:42 +0530

    implementing the default account switch

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,18 +1,54 @@
+"use client";
 import { Switch } from '@/components/ui/switch';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { ArrowUpRight, ArrowDownRight } from 'lucide-react';
 import Link from 'next/link';
+import useFetch from '@/hooks/use-fetch';
+import { updateDefaultAccount } from '@/actions/accounts';
+import { toast } from 'sonner';
 const AccountCard = ({account}) => {
     const{name,type,balance,id,isDefault}=account;
+
+    const {
+        loading:updateDefaultLoading,
+        fn:updateDefaultFn,
+        data:updatedAccount,
+        error,
+    }=useFetch(updateDefaultAccount);
+
+    const handleDefaultChange = async (event) => {
+        event.preventDefault();
+
+        if(isDefault){
+            toast.warning("You need at least one default account");
+            return;
+        }
+        await updateDefaultFn(id)
+    }
+
+    useEffect(() => {
+        if(updatedAccount?.success){
+            toast.success("Default account updated successfully");
+        }
+    },[updatedAccount,updateDefaultLoading]);
+
+    useEffect(() => {
+        if(error){
+            toast.error(error.message || "Failed to update default account");
+        }
+    },[error]);
   return (
     <Card className='hover:shadow-md transition-shadow group relative'>
-        <Link href={'/account/${id}'}>
+        <Link href={`/account/${id}`}>
         <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
             <CardTitle className='text-sm font-medium capitalize'>
                 {name}
             </CardTitle>
-            <Switch checked={isDefault}/>
+            <Switch checked={isDefault} 
+            onClick={handleDefaultChange}
+            disabled={updateDefaultLoading}
+            />
         </CardHeader>
         <CardContent>
             <div className='text-2xl font-bold'>
