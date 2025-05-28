6a1b1b09d71f5c0f7b0e86f4a3eb791f2b065013
anmolkr7 <anmolojha952@gmail.com>
2025-05-28 21:39:38 +0530

    tooltip , dropdown and chevron for sort implemented

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -1,12 +1,36 @@
 "use client";
-import React from 'react'
-import {Table, TableBody, TableCell, TableCaption, TableHead, TableHeader, TableRow} from '@/components/ui/table'
+import React,{useState} from 'react'
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table'
 import { Checkbox } from '@/components/ui/checkbox'
 import { format } from 'date-fns';
+import { categoryColors } from '@/data/categories';
+import { Badge } from '@/components/ui/badge';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { ChevronDown, ChevronUp, Clock, MoreHorizontal, RefreshCw } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { useRouter } from 'next/navigation';
+const RECURRING_INTERVALS={
+    DAILY: "Daily",
+    WEEKLY: "Weekly",
+    MONTHLY: "Monthly",
+    YEARLY: "Yearly",
+}
 const TransactionTable = ({transactions}) => {
-    const filteredAndSortedTransactions=transactions;
-    const handleSort=()=>{
+    const router=useRouter();
+    const [selectedIds,setselectedIds]=useState([]);
+    const [sortConfig, setSortConfig] = useState({
+        field:"date",
+        direction: "desc"
+    });
 
+    const filteredAndSortedTransactions=transactions;
+    const handleSort=(field)=>{
+        setSortConfig(current=>({
+            field,
+            direction:
+            current.field==field && current.direction==="asc"?"desc":"asc"
+        }))
     }
 
   return (
@@ -27,20 +51,39 @@ const TransactionTable = ({transactions}) => {
                 className="cursor-pointer"
                 onClick={()=>handleSort("date")}
                 >
-                    <div className='flex items-center'>Date</div>
+                    <div className='flex items-center'>Date{""}
+                    {sortConfig.field==='date'&&(
+                        sortConfig.direction==="asc"?
+                        (<ChevronUp className='ml-1 h-4 w-4'/>):
+                        (<ChevronDown className='ml-1 h-4 w-4'/>)
+                    )}</div>
                 </TableHead>
                 <TableHead>Description</TableHead>
                 <TableHead 
                 className="cursor-pointer"
                 onClick={()=>handleSort("category")}
                 >
-                     <div className='flex items-center'>Category</div>
+                    <div className='flex items-center'>
+                        Category
+                         {sortConfig.field==='category'&&(
+                        sortConfig.direction==="asc"?
+                        (<ChevronUp className='ml-1 h-4 w-4'/>):
+                        (<ChevronDown className='ml-1 h-4 w-4'/>)
+                    )}
+                    </div>
                 </TableHead>
                 <TableHead 
                 className="cursor-pointer"
                 onClick={()=>handleSort("amount")}
                 >
-                     <div className='flex items-center justify-end'>Amount</div>
+                    <div className='flex items-center justify-end'>
+                        Amount
+                         {sortConfig.field==='amount'&&(
+                        sortConfig.direction==="asc"?
+                        (<ChevronUp className='ml-1 h-4 w-4'/>):
+                        (<ChevronDown className='ml-1 h-4 w-4'/>)
+                    )}
+                    </div>
                 </TableHead>
                 <TableHead>Recurring</TableHead>
                 <TableHead  className="w-[50px]"/>
@@ -64,8 +107,73 @@ const TransactionTable = ({transactions}) => {
                         </TableCell>
                         <TableCell>{format(new Date(transaction.date),"PP")}</TableCell>
                         <TableCell>{transaction.description}</TableCell>
-                        <TableCell>{transaction.category}</TableCell>
-                        <TableCell className="text-right">$250.00</TableCell>
+                        <TableCell className="capitalize">
+                            <span
+                            style={{
+                                background: categoryColors[transaction.category],
+                            }}
+                            className="px-2 py-1 rounded text-white text-sm"
+                            >
+                            {transaction.category}
+                            </span>
+                        </TableCell>
+                        <TableCell className="text-right font-medium"style={{
+                            color: transaction.type === "EXPENSE" ? "red" : "green",
+                        }}>
+                            {transaction.type==="EXPENSE"?"-":"+"}
+                            ${transaction.amount.toFixed(2)}
+                        </TableCell>
+                        <TableCell>
+                            {transaction.isRecurring?(
+                                <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Badge variant="outline" 
+                                        className="gap-1 bg-purple-100 text-purple-700 hover:bg-purple-200"
+                                        >
+                                        <RefreshCw className='h-3 w-3'/>
+                                        {RECURRING_INTERVALS[transaction.recurringInterval]}
+                                        </Badge>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                    <div className='text-sm'>
+                                        <div className="font-medium">Next Date:</div>
+                                        <div>
+                                            {format(new Date(transaction.nextRecurringDate),"PP")}
+                                        </div>
+                                    </div>
+                                    </TooltipContent>
+                                </Tooltip>
+                                </TooltipProvider>
+                            ):(
+                            <Badge variant="outline" className="gap-1">
+                                <Clock className='h-3 w-3'/>
+                                One-Time
+                            </Badge>
+                            )}
+                        </TableCell>
+                        <TableCell>
+                            <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="h-8 w-8 p-0">
+                                    <MoreHorizontal className='h-4 w-4' />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel
+                                onClick={()=>
+                                    router.push(
+                                        `/transaction/create?edit=${transaction.id}`
+                                    )
+                                }
+                                >Edit</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem className="text-destructive"
+                                // onClick={()=>deleteFn([transaction.id])}
+                                >Delete</DropdownMenuItem>
+                            </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
                         </TableRow>
                     ))
                 )}
