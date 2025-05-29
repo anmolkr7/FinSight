21dd5843ce27781c528bf1fdefd297cad81c11dd
anmolkr7 <anmolojha952@gmail.com>
2025-05-29 17:44:07 +0530

    display budget component ui for default account

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,32 @@ import {Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import { getUserAccounts } from '@/actions/dashboard'
 import AccountCard from './_components/account-card'
+import { getCurrentBudget } from '@/actions/budget'
+import { BudgetProgress } from './_components/budget-progress'
+
+
+
 async function DashboardPage() {
     const accounts=await getUserAccounts();
-  return (
-    <div className='px-5'>
-        {/* BudgetProgress */}
 
+
+    const defaultAccount = accounts?.find((account) => account.isDefault);
+
+
+    // Get budget for default account
+    let budgetData = null;
+    if (defaultAccount) {
+        budgetData = await getCurrentBudget(defaultAccount.id);
+    }
+
+
+  return (
+    <div className='space-y-8'>
+        {/* BudgetProgress */}
+        {defaultAccount && (<BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+        /> )}
 
         {/* Dashboard Overview */}
 
